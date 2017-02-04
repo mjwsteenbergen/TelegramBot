235f40a205cc3d1dab5c6fb8192b53cc5fd7ab8c
mjwsteenbergen <martijnsreclame@hotmail.nl>
2017-02-04 14:03:38 +0100

    Add feedback for todo command

--- TelegramBot/Commands/TodoCommand.cs
+++ TelegramBot/Commands/TodoCommand.cs
@@ -7,9 +7,12 @@ namespace TelegramBot.Commands
     internal class TodoCommand : Command
     {
         private TodoistService ts;
+        private readonly TelegramService _tgs;
+
         public TodoCommand(TodoistService ts, TelegramService tgs) : base(tgs)
         {
             this.ts = ts;
+            _tgs = tgs;
         }
 
         public override async Task<Command> Run(string query, TgMessage m)
@@ -22,6 +25,9 @@ namespace TelegramBot.Commands
             {
                 await ts.AddTodo(query);
             }
+
+            await _tgs.SendMessage(m.from.id, "Todo was added");
+
             return null;
         }
 
