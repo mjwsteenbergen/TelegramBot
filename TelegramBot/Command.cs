403ff289a9e5a744dc3653d261cf2bf8e5bcfc34
mjwsteenbergen <martijnsreclame@hotmail.nl>
2017-04-08 23:06:29 +0200

    Started on implementing CommandLine in telegrambot

--- TelegramBot/Command.cs
+++ TelegramBot/Command.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using ApiLibs.Telegram;
+using CommandLine;
+using CommandLine.Text;
 
 namespace TelegramBot
 {
@@ -21,14 +26,116 @@ namespace TelegramBot
 
         public virtual Task<Command> Run(string query, TgInlineQuery m)
         {
-            return null;
+            return Task.FromResult((Command) null);
         }
 
         public virtual Task<Command> Run(string query, ChosenInlineResult m)
         {
-            return null;
+            return Task.FromResult((Command) null);
         }
 
         public abstract string CommandName { get; }
+    }
+
+    public abstract class Command<T> : Command where T : new()
+    {
+        public Command(TelegramService tgs) : base(tgs) { }
+
+        public override Task<Command> Run(string query, TgMessage m)
+        {
+            var result = Parser.Default.ParseArguments<T>(query.Split());
+            Command o = null;
+            Task t = new Task(() => {});
+            result.WithNotParsed(errs =>
+            {
+                OnError(m, result, errs);
+                t.Start();
+            });
+            result.WithParsed(async args =>
+            {
+                o = await Run(args, m);
+                t.Start();
+            });
+            t.Wait();
+            return Task.FromResult(o);
+        }
+
+        private void OnError(TgMessage m, ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            HelpText.AutoBuild(result, h =>
+            {
+                h.AddOptions(result);
+                h.Heading = "";
+                h.Copyright = "";
+                Task.Run(async () =>
+                {
+                    await _tgs.SendMessage(m.from.id, "*I was unable to execute this request:*\n" + SentenceBuilder.Factory().FormatError(errs.ElementAt(0)), ParseMode.Markdown);
+                }).Wait();
+                return h;
+            }, e => e);
+        }
+
+        public override Task<Command> Run(string query, TgInlineQuery m)
+        {
+            var result = Parser.Default.ParseArguments<T>(query.Split());
+            Command o = null;
+            Task t = new Task(() => { });
+            result.WithNotParsed(errs =>
+            {
+                OnError(m, result, errs);
+                t.Start();
+            });
+            result.WithParsed(async args =>
+            {
+                o = await Run(args, m);
+                t.Start();
+            });
+            t.Wait();
+            return Task.FromResult(o);
+        }
+
+        private void OnError(TgInlineQuery m, ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            HelpText.AutoBuild(result, h =>
+            {
+                h.AddOptions(result);
+                h.Heading = "";
+                h.Copyright = "";
+                Task.Run(async () =>
+                {
+                    await _tgs.answerInlineQuery(m.id, new List<InlineQueryResultArticle>
+                    {
+                        new InlineQueryResultArticle
+                        {
+                            id = "Error",
+                            input_message_content = new InputTextMessageContent(){ message_text = SentenceBuilder.Factory().FormatError(errs.ElementAt(0))},
+                            title = SentenceBuilder.Factory().FormatError(errs.ElementAt(0))
+                        }
+                    });
+                }).Wait();
+                return h;
+            }, e => e);
+        }
+
+        public Task<Command> Run(string query, ChosenInlineResult m)
+        {
+            return null;
+        }
+
+        public virtual async Task<Command> Run(T query, TgMessage m)
+        {
+            await _tgs.SendMessage(m.from.id, "This is not supported for this command");
+            return null;
+        }
+
+        public virtual Task<Command> Run(T query, TgInlineQuery m)
+        {
+            return null;
+        }
+
+        public virtual Task<Command> Run(T query, ChosenInlineResult m)
+        {
+            return null;
+        }
     }
 }
