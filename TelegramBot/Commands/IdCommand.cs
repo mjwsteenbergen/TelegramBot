ccc39b086dfb9e3d833312ebfdebb4f72b706f2d
mjwsteenbergen <martijnsreclame@hotmail.nl>
2017-10-14 19:33:27 +0200

    Started work

---
+++ TelegramBot/Commands/IdCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiLibs.Telegram;
+
+namespace TelegramBot.Commands
+{
+    class IdCommand : Command
+    {
+        public IdCommand(TelegramService tgs) : base(tgs)
+        {
+
+        }
+
+        public override async Task<Command> Run(string query, TgMessage m)
+        {
+            await _tgs.SendMessage(m.chat.id, m.from.id.ToString());
+            return null;
+        }
+
+        public override string CommandName => "getId";
+    }
+}
